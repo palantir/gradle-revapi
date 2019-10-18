06f895d6c9f6fd9e50b5852e10db7c64d28c2765
Callum Rogers <callumr@palantir.com>
2019-10-18 14:17:33 +0100

    Don't use the circle 1 CIRCLE_TEST_REPORTS variable to put the junit reports in the right place

--- src/main/java/com/palantir/gradle/revapi/RevapiReportTask.java
+++ src/main/java/com/palantir/gradle/revapi/RevapiReportTask.java
@@ -25,7 +25,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
-import java.util.Optional;
 import org.gradle.api.tasks.TaskAction;
 
 public class RevapiReportTask extends RevapiJavaTask {
@@ -85,10 +84,6 @@ public class RevapiReportTask extends RevapiJavaTask {
     }
 
     private File junitOutput() {
-        Optional<String> circleReportsDir = Optional.ofNullable(System.getenv("CIRCLE_TEST_REPORTS"));
-        File reportsDir = circleReportsDir
-                .map(File::new)
-                .orElseGet(() -> getProject().getBuildDir());
-        return new File(reportsDir, "junit-reports/revapi/revapi-" + getProject().getName() + ".xml");
+        return new File(getProject().getBuildDir(), "junit-reports/revapi/revapi-" + getProject().getName() + ".xml");
     }
 }
