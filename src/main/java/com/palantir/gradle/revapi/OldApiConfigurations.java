218bdc97767e220b07b6b5731b97e033301533fc
Callum Rogers <callumr@palantir.com>
2019-11-20 16:04:29 +0000

    Fix too many configurations

--- src/main/java/com/palantir/gradle/revapi/OldApiConfigurations.java
+++ src/main/java/com/palantir/gradle/revapi/OldApiConfigurations.java
@@ -36,10 +36,12 @@ final class OldApiConfigurations {
 
         Dependency oldApiDependency = project.getDependencies().create(groupNameVersion.asString());
 
+        String transitivityString = transitive ? "transitive" : "";
+
         Configuration oldApiConfiguration = OldApiConfigurations.configuration(
                 project,
                 oldApiDependency,
-                "revapiOldApi_" + groupNameVersion.version().asString());
+                "revapiOldApi_" + groupNameVersion.version().asString() + "_" + transitivityString);
         oldApiConfiguration.setTransitive(transitive);
 
         return PreviousVersionResolutionHelpers.withRenamedGroupForCurrentThread(project, () ->
