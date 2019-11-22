09ada5d28e5c856327088ecfe83a9fa507e035b6
Callum Rogers <callumr@palantir.com>
2019-11-22 15:30:35 +0000

    Remove unneeded serializations

--- src/main/java/com/palantir/gradle/revapi/config/Version.java
+++ src/main/java/com/palantir/gradle/revapi/config/Version.java
@@ -18,11 +18,9 @@ package com.palantir.gradle.revapi.config;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import org.immutables.serial.Serial;
 import org.immutables.value.Value;
 
 @Value.Immutable
-@Serial.Structural
 public interface Version {
     @JsonValue
     String asString();
